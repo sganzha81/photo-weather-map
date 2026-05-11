8b9504c7eff201cffa9002915328472d16035960
sganzha81 <sganzha@gmail.com>
2026-05-12 01:43:32 +0400

    Add GeoJSON endpoint and fix requests version

--- photos/urls.py
+++ photos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('data.json', views.photos_geojson, name='photos_geojson'),
     path("", views.photo_list, name="photo_list"),
     path("upload/", views.upload_photo, name="upload_photo"),
     path("place/", views.get_place_name, name="get_place_name"),
